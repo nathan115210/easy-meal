75b80e7db4207f5e6a81a7eaeade4b0f24c397d7
Hongyu.Zhao <nathan_115210@hotmail.com>
2025-02-08 04:26:56 +0200

    fix typeError

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,16 +3,15 @@
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
-
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
+import { markdownSchema } from "sanity-plugin-markdown";
 import { structureTool } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schemaTypes";
 import { structure } from "./sanity/structure";
-import { markdownSchema } from "sanity-plugin-markdown";
 
 export default defineConfig({
   basePath: "/studio",
